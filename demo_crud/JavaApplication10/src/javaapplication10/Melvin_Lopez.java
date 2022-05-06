43340f2861293f89cb7ac2ba080fa138e991667b
melvinantonio <melvinantonioangellopez@gmail.com>
2022-05-06 14:49:36 -0600

    Cambios

--- demo_crud/JavaApplication10/src/javaapplication10/Melvin_Lopez.java
+++ demo_crud/JavaApplication10/src/javaapplication10/Melvin_Lopez.java
@@ -2,4 +2,11 @@ package javaapplication10;
 
 public class Melvin_Lopez {
    
+    public class vehiculoCamioneta extends Vehiculo{
+    private int nPuertas;
+    public vehiculoCamioneta(String marca, String color, int anio, int nLLantas) {
+        super(marca, color, anio, nLLantas);
+    }
+    
+    }
 }
